f4c37558718e6fe708b2e37fc3aa22510e1dc41a
Arjun Sarode <sarodearjun57@gmail.com>
2015-05-22 06:06:09 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+	setStorageFile: function(pathToFile) {
+
+	},
+
+	getStorageFile: function() {
+
+	},
+
+	getStorageFileName: function() {
+
+	},
+
+	setItem: function(key, value) {
+
+	},
+
+	getItem: function(key) {
+
+	},
+
+	removeItem: function(key) {
+
+	},
+
+	setItemKey: function(key, newKey) {
+
+	},
+
+	incrementItem: function(key) {
+
+	},
+
+	pushItem: function(key) {
+
+	},
+
+	clear: function() {
+
+	}
+}
