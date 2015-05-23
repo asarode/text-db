fe409bd9a870b037c3ec54087d4003434dbb95d3
Arjun Sarode <sarodearjun57@gmail.com>
2015-05-23 02:29:08 -0500

    add non-enumerable  _keys field

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ var path = require('path');
 
 module.exports = function(dir) {
 	var dir = dir || path.join(process.cwd(), 'storage');
-	var file = path.join(dir, 'storage.json');
+	var file = path.join(dir, '_storage.json');
 	var dataObj = {};
+	Object.defineProperty(dataObj, "_keys", {
+		enumerable: false,
+		configurable: false,
+		writable: true,
+		value: []
+	});
 	fs.readFile(file, function(err, data) {
 		dataObj = JSON.parse(data);
 	});
@@ -20,6 +26,11 @@ module.exports = function(dir) {
 		},
 
 		setItem: function(key, value) {
+			if (dataObj["_keys"] === undefined) {
+				dataObj["_keys"] = [key];
+			} else {
+				dataObj["_keys"].push(key);
+			}
 			dataObj[key] = value;
 			var dataObjStr = JSON.stringify(dataObj);
 
@@ -32,24 +43,55 @@ module.exports = function(dir) {
 			return dataObj[key];
 		},
 
+		getKeys: function() {
+			return dataObj["_keys"].slice(0);
+		},
+
+		getLength: function() {
+			return dataObj["_keys"].length;
+		},
+
 		removeItem: function(key) {
-
+			delete dataObj[key];
 		},
 
-		setItemKey: function(key, newKey) {
+		// incrementItem: function(key) {
+		// 	var value = dataObj[key];
+		// 	if (typeof value == "number") {
+		// 		value++;
+		// 	} else if (!(key in dataObj)) {
+		// 		throw new Error("Key doesn't exist.");
+		// 	} else {
+		// 		throw new Error("Can't increment non-number values.");
+		// 	}
+		// },
 
-		},
+		// decrementItem: function(key) {
+		// 	var value = dataObj[key];
+		// 	if (typeof value == "number") {
+		// 		value--;
+		// 	} else if (!(key in dataObj)) {
+		// 		throw new Error("Key doesn't exist.");
+		// 	} else {
+		// 		throw new Error("Can't increment non-number values.");
+		// 	}
 
-		incrementItem: function(key) {
+		// },
 
-		},
-
-		pushItem: function(key) {
-
-		},
+		// pushItem: function(key, data) {
+		// 	var value = dataObj[key];
+		// 	if (value.prototype === Array) {
+		// 		value.push(data);
+		// 	} else if (!(key in dataObj)) {
+		// 		throw new Error("Key doesn't exist.");
+		// 	} else {
+		// 		throw new Error("Can't push item into non-array value.");
+		// 	}
+		// },
 
 		clear: function() {
 			dataObj = {};
+			dataKets = [];
 			fs.unlink(file, function(err) {
 				if (err) throw err;
 			});
--- test/testStorage.js
+++ test/testStorage.js
@@ -1,21 +1,51 @@
-var assert = require('assert');
-var db = require('./../index')('storage');
-var fs = require('fs');
+var assert = require("assert");
+var db = require("./../index")("storage");
+var fs = require("fs");
 
-suite('text-db', function() {
-	test('getStorageFile() shows correct file', function(done) {
-		assert.equal('storage/storage.json', db.getStorageFile());
+suite("text-db", function() {
+	test("getStorageFile() shows correct file", function(done) {
+		assert.equal("storage/_storage.json", db.getStorageFile());
 		done();
 	});
-	test('setItem() sets the kay-value data', function(done) {
+	test("setItem() and getItem() set and get correct data", function(done) {
 		var key = "test";
 		var value = "monkey";
 		db.setItem(key, value);
-		var result = db.getItem(key);
-		assert.equal(value, result);
+		assert.equal(value, db.getItem(key));
+		db.clear();
 		done();
 	});
-	test('file deleted on clear()', function(done) {
+	test("setItem() modify dataKeys correctly", function(done) {
+		var _keys = ["test1", "test2", "test3"];
+		var value = "monkey";
+		_keys.forEach(function(item, index) {
+			db.setItem(_keys[index], value);
+		});
+		var keys = db.getKeys();
+		keysAreEqual = true;
+		if (keys.length != _keys.length) {
+			keysAreEqual = false;
+		}
+		keys.forEach(function(item, index) {
+			if (keys[index] != _keys[index]) {
+				keysAreEqual = false;
+			}
+		});
+		assert.equal(true, keysAreEqual);
+		db.clear();
+		done();
+	});
+	test("getLength() returns correct length", function(done) {
+		var keys = ["test1", "test2", "test3"];
+		var value = "monkey";
+		keys.forEach(function(item, index) {
+			db.setItem(keys[index], value);
+		});
+		assert.equal(keys.length, db.getLength());
+		db.clear();
+		done();
+	});
+	test("file deleted on clear()", function(done) {
 		var key = "test";
 		var value = "monkey";
 		db.setItem(key, value);
